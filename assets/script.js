19b7822c3fbcd8951bcab27de9bc292d490ca12d
Rai Dorzback <raidorzback@gmail.com>
2026-04-09 17:11:32 -0400

    add notes to javascript, finish marking up html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 // TO DO: code to make drag and drop work. Refer to https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+
+// TO DO: make it so that clear and change buttons appear after file is uploaded
+
+// TO DO: error handling for all form inputs 
+
+// TO DO: handle form submission
