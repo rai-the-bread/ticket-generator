7dfbef455e35e41205fed712dbb882c473cc87cd
Rai Dorzback <raidorzback@gmail.com>
2026-05-21 14:16:32 -0400

    show preview of avatar and buttons when file is uplaoded

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,35 @@
 const ticketForm = document.querySelector("#ticket-form");
-const avatarPreview = document.querySelector("#preview");
+const avatarPreview = document.querySelector("#avatar-preview");
+const avatarInput = document.querySelector("#avatar");
+const previewButtons = document.querySelector(".preview-buttons");
+const dropZoneIcon = document.querySelector(".drop-zone-icon");
+const dropZoneText = document.querySelector(".drop-text");
 
 // TO DO: code to make drag and drop work. Refer to https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
 
 // TO DO: when a file is uploaded, show a preview of the avatar and show the Remove Image and Change Image buttons
+function showPreview(event) {
+    console.log("show preview function running")
+    
+    // get avatar image and convert to url
+    let avatarFile = event.target.files[0];
+    let avatarHtml = '';
+    if (avatarFile && avatarFile.size > 0) {
+        let avatarURL = URL.createObjectURL(avatarFile);
+        avatarHtml = `<img src="${avatarURL}" alt="Avatar" class="avatar-preview">`;
+    }
+
+    // show preview of the avatar
+    avatarPreview.innerHTML = avatarHtml;
+
+    // remove any existing items in drag and drop box
+    dropZoneIcon.classList.add("hidden");
+    dropZoneText.classList.add("hidden");
+
+    // show remove image button
+    previewButtons.classList.remove("hidden");
+    // show change image button
+}
 
 // TO DO: error handling for all form inputs 
 
@@ -15,7 +41,6 @@ function onSubmit(event) {
     const dataObject = Object.fromEntries(data.entries());
     console.log(dataObject)
     const { fullName, email, github, avatarHtml } = getTicketData(dataObject);
-    avatarPreview.innerHTML = avatarHtml;
 	ticketForm.reset();
 };
 
@@ -35,6 +60,6 @@ function getTicketData(dataObject) {
     return { fullName, email, github, avatarHtml };
 }
 
+// event listeners
 ticketForm.addEventListener("submit", onSubmit);
-
-// {avatar: File, full-name: 'Rai Dorzback', email: 'rai@anniecannons.com', github: 'rai-the-bread'}
+avatarInput.addEventListener("change", showPreview);
