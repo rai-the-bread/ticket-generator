6d8cb663ad288ab15d7445b42f8584d287e0503d
Rai Dorzback <raidorzback@gmail.com>
2026-05-21 14:25:12 -0400

    drag and drop functionality

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,17 @@ const avatarInput = document.querySelector("#avatar");
 const previewButtons = document.querySelector(".preview-buttons");
 const dropZoneIcon = document.querySelector(".drop-zone-icon");
 const dropZoneText = document.querySelector(".drop-text");
+const dropZone = document.querySelector("#drop-zone");
 
-// TO DO: code to make drag and drop work. Refer to https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+// drag and drop handler
+function dropHandler(event) { 
+    console.log("drop handler running");
+    event.preventDefault();
+    avatarInput.files = event.dataTransfer.files;
+    showPreview({ target: avatarInput });
+};
 
-// TO DO: when a file is uploaded, show a preview of the avatar and show the Remove Image and Change Image buttons
+// when a file is uploaded, show a preview of the avatar and show the Remove Image and Change Image buttons
 function showPreview(event) {
     console.log("show preview function running")
     
@@ -40,7 +47,9 @@ function onSubmit(event) {
 	const data = new FormData(event.target);
     const dataObject = Object.fromEntries(data.entries());
     console.log(dataObject)
-    const { fullName, email, github, avatarHtml } = getTicketData(dataObject);
+    // save variables from form
+    const { fullName, email, github, avatarURL } = getTicketData(dataObject);
+    console.log('form data:', fullName, email, github, avatarURL);
 	ticketForm.reset();
 };
 
@@ -51,15 +60,47 @@ function getTicketData(dataObject) {
     // get image file that was uploaded, convert to a URL
     let avatarFile = dataObject.avatar;
     let avatarHtml = '';
+    let avatarURL = '';
     if (avatarFile && avatarFile.size > 0) {
-        let avatarURL = URL.createObjectURL(avatarFile);
-        avatarHtml = `<img src="${avatarURL}" alt="${fullName} Avatar" class="avatar-preview">`;
+        avatarURL = URL.createObjectURL(avatarFile);
     }
-    // testing
-    preview.innerHTML = avatarHtml;
-    return { fullName, email, github, avatarHtml };
+    // return data from form
+    return { fullName, email, github, avatarURL };
 }
 
 // event listeners
 ticketForm.addEventListener("submit", onSubmit);
 avatarInput.addEventListener("change", showPreview);
+dropZone.addEventListener("drop", dropHandler);
+
+// Boilerplate code from https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop to prevent default behavior for drag and drop
+window.addEventListener("drop", (e) => {
+  if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
+    e.preventDefault();
+  }
+});
+dropZone.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (fileItems.some((item) => item.type.startsWith("image/"))) {
+      e.dataTransfer.dropEffect = "copy";
+    } else {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+window.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (!dropZone.contains(e.target)) {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
